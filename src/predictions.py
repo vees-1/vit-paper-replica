bf8ece433716203e0efd0d0f209c0c621dbb6b19
Vineet <vineet@Vineets-MacBook-Pro.local>
2026-02-25 22:06:41 +0530

    modified

--- src/predictions.py
+++ src/predictions.py
@@ -1,8 +1,4 @@
-"""
-Utility functions to make predictions.
-
-Main reference for code creation: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set 
-"""
+"""Utility functions to make predictions."""
 import torch
 import torchvision
 from torchvision import transforms
@@ -12,11 +8,8 @@ from typing import List, Tuple
 
 from PIL import Image
 
-# Set device
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Predict on a target image with a target model
-# Function created in: https://www.learnpytorch.io/06_pytorch_transfer_learning/#6-make-predictions-on-images-from-the-test-set
 def pred_and_plot_image(
     model: torch.nn.Module,
     class_names: List[str],
@@ -25,21 +18,10 @@ def pred_and_plot_image(
     transform: torchvision.transforms = None,
     device: torch.device = device,
 ):
-    """Predicts on a target image with a target model.
+    """Predicts on a target image with a trained model and plots it."""
 
-    Args:
-        model (torch.nn.Module): A trained (or untrained) PyTorch model to predict on an image.
-        class_names (List[str]): A list of target classes to map predictions to.
-        image_path (str): Filepath to target image to predict on.
-        image_size (Tuple[int, int], optional): Size to transform target image to. Defaults to (224, 224).
-        transform (torchvision.transforms, optional): Transform to perform on image. Defaults to None which uses ImageNet normalization.
-        device (torch.device, optional): Target device to perform prediction on. Defaults to device.
-    """
-
-    # Open image
     img = Image.open(image_path)
 
-    # Create transformation for image (if one doesn't exist)
     if transform is not None:
         image_transform = transform
     else:
@@ -53,27 +35,19 @@ def pred_and_plot_image(
             ]
         )
 
-    ### Predict on image ###
 
-    # Make sure the model is on the target device
     model.to(device)
 
-    # Turn on model evaluation mode and inference mode
     model.eval()
     with torch.inference_mode():
-        # Transform and add an extra dimension to image (model requires samples in [batch_size, color_channels, height, width])
         transformed_image = image_transform(img).unsqueeze(dim=0)
 
-        # Make a prediction on image with an extra dimension and send it to the target device
         target_image_pred = model(transformed_image.to(device))
 
-    # Convert logits -> prediction probabilities (using torch.softmax() for multi-class classification)
     target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
 
-    # Convert prediction probabilities -> prediction labels
     target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
 
-    # Plot image with predicted label and probability
     plt.figure()
     plt.imshow(img)
     plt.title(
